34b54f600dbf2cf2f4f462c8c0300e9fc4b10599
Justin Cripps <justin@justincripps.com>
2024-10-16 15:41:21 +1000

    Update mush_commands.py
    Bug fixes.

--- mush_commands.py
+++ mush_commands.py
@@ -5,8 +5,7 @@ def handle_command(command, client_socket, clients, channels, channel_config_fil
         client_socket.close()
     elif command.startswith('/who'):
         users = [f'Client {i+1}' for i in range(len(clients))]
-        client_socket.send(f'Connected users: {", ".join(users)}
-'.encode('utf-8'))
+        client_socket.send(f'Connected users: {", ".join(users)}'.encode('utf-8'))
     elif command.startswith('/addchannel '):
         # Add a new channel
         parts = command.split(' ', 2)
@@ -27,13 +26,11 @@ def handle_command(command, client_socket, clients, channels, channel_config_fil
         with open(channel_config_file, 'a') as config_file:
             config_file.write(f'{channel_name} = {display_name}\n')
         
-        client_socket.send(f'Channel "{display_name}" added successfully.
-'.encode('utf-8'))
+        client_socket.send(f'Channel "{display_name}" added successfully.'.encode('utf-8'))
     elif command.startswith('/listchannels'):
         # List all chat channels
         channel_list = [f'{name} ({display_name})' for name, display_name in channels.items()]
-        client_socket.send(f'Available channels: {", ".join(channel_list)}
-'.encode('utf-8'))
+        client_socket.send(f'Available channels: {", ".join(channel_list)}'.encode('utf-8'))
     elif command.startswith('/removechannel '):
         # Remove a channel
         parts = command.split(' ', 1)
@@ -57,20 +54,16 @@ def handle_command(command, client_socket, clients, channels, channel_config_fil
                 if not line.startswith(f'{channel_name} = '):
                     config_file.write(line)
         
-        client_socket.send(f'Channel "{channel_name}" removed successfully.
-'.encode('utf-8'))
+        client_socket.send(f'Channel "{channel_name}" removed successfully.'.encode('utf-8'))
     elif command.startswith('/look'):
         # Look around the current room
         room_id = player_rooms.get(client_socket, "1")
         room = rooms.get(room_id)
         if room:
-            client_socket.send(f'You are in {room["name"]}.
-{room["description"]}
-'.encode('utf-8'))
+            client_socket.send(f'You are in {room["name"]}. {room["description"]}'.encode('utf-8'))
             if room["exits"]:
                 exits = ', '.join(room["exits"].keys())
-                client_socket.send(f'Exits: {exits}
-'.encode('utf-8'))
+                client_socket.send(f'Exits: {exits}'.encode('utf-8'))
         else:
             client_socket.send(b'You are in an unknown place.\n')
     elif command.startswith('/go '):
@@ -86,9 +79,7 @@ def handle_command(command, client_socket, clients, channels, channel_config_fil
             new_room_id = current_room["exits"][direction]
             player_rooms[client_socket] = new_room_id
             new_room = rooms.get(new_room_id)
-            client_socket.send(f'You move {direction} and arrive in {new_room["name"]}.
-{new_room["description"]}
-'.encode('utf-8'))
+            client_socket.send(f'You move {direction} and arrive in {new_room["name"]}. {new_room["description"]}'.encode('utf-8'))
         else:
             client_socket.send(b'You cannot go that way.\n')
     elif command.startswith('/help'):
